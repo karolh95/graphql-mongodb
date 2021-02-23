a4494136aca25fbcb94d4a3ebfe0054ea2539e55
Karol Haniszewski <k.haniszewski@doittechnology.pl>
2021-02-23 14:34:09 +0100

    Get All Students GraphQL Query

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { StudentType } from './student.type';
 import { CreateStudentInput } from './create-student.input';
 import { StudentService } from './student.service';
@@ -9,6 +9,11 @@ export class StudentResolver {
 	constructor(
 		private studentService: StudentService
 	) { }
+
+	@Query(result => [StudentType])
+	async students() {
+		return this.studentService.getStudents();
+	}
 
 	@Mutation(returns => StudentType)
 	async createStudent(
